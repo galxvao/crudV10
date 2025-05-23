f506c2faab825f5af92f5b1db71b6cb0af1b2f44
DAVI ALEX PAIVA GALVAO <davi_galvao@estudante.sc.senai.br>
2025-05-22 21:05:47 -0300

    Adicionando controller de Contato

--- src/main/java/com/senai/crud/controllers/ContatoController.java
+++ src/main/java/com/senai/crud/controllers/ContatoController.java
@@ -1,7 +1,58 @@
 package com.senai.crud.controllers;
 
+import com.senai.crud.dtos.ContatoDto;
+import com.senai.crud.dtos.ProdutoDto;
+import com.senai.crud.services.ContatoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/contato")
 public class ContatoController {
+
+    @Autowired
+    ContatoService service;
+
+
+    @PostMapping
+    public String adicionar (@ModelAttribute ("contatoDto") ContatoDto contatoDto ) {
+
+        boolean retorno = service.adicionar(contatoDto);
+
+
+        if (retorno){
+            return "redirect:/contatolista";
+        }
+
+        return "redirect:/contatocadastro?erro";
+    }
+
+    @PostMapping("/{id}")
+    public String atualizar(@ModelAttribute("contatoDto") ContatoDto contatoDto, @PathVariable Long id){
+
+        boolean retorno = false;
+
+        retorno = service.atualizar(id, contatoDto);
+
+        if(retorno) {
+            return "redirect:/contatolista";
+        }
+
+        return "redirect:/contatoatualizar/" + id.toString() + "?erro";
+    }
+
+
 
 
 
 }
+
+
+
+
+
+
--- src/main/java/com/senai/crud/services/ContatoService.java
+++ src/main/java/com/senai/crud/services/ContatoService.java
@@ -1,14 +1,18 @@
 package com.senai.crud.services;
 
 
+import com.senai.crud.dtos.CategoriaDto;
 import com.senai.crud.dtos.ContatoDto;
+import com.senai.crud.models.CategoriaModel;
 import com.senai.crud.models.ContatoModel;
+import com.senai.crud.models.ProdutoModel;
 import com.senai.crud.repositories.ContatoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ContatoService {
@@ -47,8 +51,31 @@ public class ContatoService {
 
     }
 
+    public boolean atualizar (Long id, ContatoDto contatoDto){
 
+        Optional<ContatoModel> contatoOptional = repository.findById(id);
 
+        if (contatoOptional.isPresent()){
+            contatoOptional.get().setId(contatoDto.getId());
+            contatoOptional.get().setNome(contatoDto.getNome());
+            contatoOptional.get().setSigla(contatoDto.getSigla());
+
+            repository.save(contatoOptional.get());
+            return true;
+        }
+        return false;
+    }
+
+    public boolean remover(Long id){
+        Optional<ContatoModel> contatoOptional = repository.findById(id);
+
+        if(contatoOptional.isPresent()){
+            repository.delete(contatoOptional.get());
+            return true;
+        }
+
+        return false;
+    }
 
 
 
